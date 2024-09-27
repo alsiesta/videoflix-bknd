bf4cdcb17cd7dd45301e1882e42a0753e5810544
alsiesta <as@daslizenzatelier.de>
2024-09-27 17:25:14 +0200

    fixed start.sh

--- start.sh
+++ start.sh
@@ -1,57 +1,69 @@
 #!/bin/bash
 
-echo "Updating package list..."
+echo "----------- Updating package list..."
 sudo apt update
 
-echo "Upgrading installed packages..."
+echo "----------- Upgrading installed packages..."
 sudo apt upgrade -y
 
-echo "Installing PostgreSQL and contrib package..."
-sudo apt install -y postgresql postgresql-contrib
+echo "----------- Installing PostgreSQL and contrib package..."
+sudo apt install -y postgresql postgresql-contrib expect
 
-echo "Setting PostgreSQL default user password..."
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'Test123';"
-
-echo "Starting PostgreSQL service..."
+echo "----------- Starting PostgreSQL service..."
 sudo service postgresql start
 
-echo "Creating PostgreSQL database 'videoflix_db'..."
-sudo -u postgres CREATE DATABASE videoflix_db
+echo "----------- Setting PostgreSQL default user password..."
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'Test123';"
 
-echo "Listing PostgreSQL databases..."
-sudo "\l"
-\q
+echo "----------- Creating PostgreSQL database 'videoflix_db'..."
+sudo -u postgres psql -c "CREATE DATABASE videoflix_db;"
 
-echo "Creating virtual environment..."
+echo "----------- Listing PostgreSQL databases..."
+sudo -u postgres psql -c "\l"
+
+echo "----------- Creating virtual environment..."
 # Create virtual environment
 python3 -m venv myvenv
 
-echo "Activating virtual environment..."
+echo "----------- Activating virtual environment..."
 # Activate virtual environment
 source myvenv/bin/activate
 
-echo "Installing requirements..."
+echo "----------- Installing requirements..."
 # Install requirements
 pip install -r requirements.txt
 
-echo "Starting RQ worker..."
-# While Redis is automatically in Ubuntu the rq worker needs to be started manually
-python3 manage.py rqworker default
-
-echo "Create database tables..."
+echo "----------- Create database tables..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "Creating superuser..."
-expect << EOF
-spawn python3 manage.py createsuperuser --username Alex --email ""
-expect "Password:"
-send "12\r"
-expect "Password (again):"
-send "12\r"
-expect "Bypass password validation and create user anyway? (y/N)"
-send "y\r"
-expect eof
-EOF
+echo "----------- Creating superuser..."
+
+echo "----------- Please create a superuser for Django admin interface"
+echo "----------- Add Name, Email and Password when prompted"
+python3 manage.py createsuperuser
+
+echo "----------- Starting RQ worker in a separate shell..."
+# Start RQ worker in a separate shell
+nohup bash -c "python3 manage.py rqworker default" > rqworker.log 2>&1 &
+
+echo "----------- Checking if port 8000 is in use..."
+if lsof -i:8000; then
+  echo "----------- Port 8000 is in use. Killing the process..."
+  fuser -k 8000/tcp
+fi
+
+echo "----------- Starting Django development server..."
+python3 manage.py runserver &
+
+# Wait for the server to start
+echo "----------- Waiting for Django development server to start..."
+while ! nc -z localhost 8000; do   
+  sleep 1 # wait for 1 second before checking again
+done
+
+echo "----------- Opening browser to Django admin interface..."
+# Use explorer.exe to open the browser in Windows
+explorer.exe "http://127.0.0.1:8000/admin" &
 
 echo "All tasks completed."
