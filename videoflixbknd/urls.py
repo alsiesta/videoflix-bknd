10f2d5d6e212d8434eba7f0ee9ba892504de82fc
alsiesta <as@daslizenzatelier.de>
2024-10-16 21:52:43 +0200

    edit .env var for prod and dev

--- videoflixbknd/urls.py
+++ videoflixbknd/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
 ]+ debug_toolbar_urls() + staticfiles_urlpatterns()
 
 
-# if settings.DEBUG:
-#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
